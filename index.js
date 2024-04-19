029267eb4f62a2686f682ca6231c3fbed583d88b
MarCrivelli <marcoantonio7crivelli@gmail.com>
2024-04-18 21:33:26 -0300

    Update 3
    Aparentemente, tudo funcionando. Está havendo um erro que não sei resolver, com a descrição: "SequelizeConnectionError: autentica��o do tipo senha falhou para o usu�rio "postgres"".

--- index.js
+++ index.js
@@ -3,6 +3,56 @@ const conn = require("./db/conn");
 
 const Usuario = require("./models/Usuario");
 
+const express = require ("express");
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.get("/usuarios/novo", (req, res) => {
+  res.sendFile('${__dirname}/views/formUsuario.html');
+});
+
+app.post("/usuarios/novo", async (req, res) => {
+  const nickname = req.body.nickname;
+  const nome = req.body.nome;
+
+  const dadosUsuario = {
+    nickname,
+    nome,
+  };
+  const usuario = await Usuario.create(dadosUsuario);
+  res.send("Usuário inserido sob o id " + usuario.id);
+});
+
+
+app.get("/jogos/novo", (req, res) => {
+  res.sendFile('${__dirname}/views/formJogo.html');
+});
+
+app.post("/jogos/novo", async (req, res) => {
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  const precoBase = req.body.precoBase;
+
+  const dadosJogo = {
+    titulo,
+    descricao,
+    precoBase,
+  };
+  const jogo = await Usuario.create(dadosJogo);
+  res.send("Jogo inserido sob o id " + jogo.id);
+});
+
+
+app.listen(8000, () => {
+  console.log("Server rodando na porta 8000!");
+});
+
 conn 
  .sync()
   .then(() => {
